aec8d4162de3f7f154906d54d33b7f5a41fc4e7c
TaeHyong Kim <kthstar27@gmail.com>
2023-07-18 12:49:32 +0900

    1.0

--- RECORD/variables.tf
+++ RECORD/variables.tf
@@ -4,7 +4,7 @@ variable "DOMAINs" {
 }
 
 variable "DNSs" {
-    type = list(object({
+    type = list({
         RECORDs = list(object({
             DOMAIN = string
             NAME = string
@@ -12,7 +12,7 @@ variable "DNSs" {
             VALUE = string
             TTL = string
         }))
-    }))
+    })
     default = {
         RECORDs = []
     }
